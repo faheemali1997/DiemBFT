1c295a78234bae58b5656a33da833104fbd8c43c
faheemali1997 <fmali1997@gmail.com>
2021-10-22 18:56:08 -0400

    fix: config changes

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ configs_four_node = {
 configs_seven_node = {
     'num_replicas': 7,
     'num_clients': 3,
-    'timer_constant': 4,
+    'timer_constant': 1,
     'seed_value': 10,
     'num_byzentine_nodes': 2,
     'client_retry_timeout': 10
